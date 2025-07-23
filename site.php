a3725bbb9aaea8f5e9a71b4101de69316b068ede
Felipe Steffen <frfelipe22@gmail.com>
2025-07-23 16:38:51 -0300

    criacção calculo de frete e totais do carrinho

--- site.php
+++ site.php
@@ -62,7 +62,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", array(
 		"cart" => $cart->getValues(),
-		"products" => $cart->getProducts()
+		"products" => $cart->getProducts(),
+		"error" => ''
 	));
 });
 
@@ -102,6 +103,15 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	$cart = Cart::getFromSession();
 
 	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+});
+
+$app->post("/cart/freight", function(){
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
 
 	header("Location: /cart");
 	exit;
--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -92,6 +92,8 @@ class Cart extends Model {
             ":idproduct" => $product->getidproduct()
         ));
 
+        $this->getCalculateTotal();
+
     }
 
     public function removeProduct(Product $product, $all = false){
@@ -108,6 +110,8 @@ class Cart extends Model {
                 ":idproduct" => $product->getidproduct()
             ));
         }
+
+        $this->getCalculateTotal();
     }
 
     public function getProducts(){
@@ -127,6 +131,85 @@ class Cart extends Model {
             ));
 
         return Product::checkList($results);
+    }
+
+    public function getProductsTotals(){
+        $sql = new Sql();
+
+        $results = $sql->select(
+            "SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheight, SUM(vllength) AS vllength, SUM(vlweight) AS vlweight, COUNT(*) AS nrqtd
+            FROM tb_products p
+            INNER JOIN tb_cartsproducts cp
+                ON p.idproduct = cp.idproduct
+            WHERE cp.idcart = :idcart AND dtremoved IS NULL"
+            , array(
+                ":idcart" => $this->getidcart()
+            ));
+        
+        if(!empty($results))
+            return $results[0];
+        
+        return [];
+    }
+
+    public function setFreight($nrzipcode){
+        /*
+        /////////// Não funciona mais este link para calculo de frete dos Correios, colocado valores fixos de momento
+        $nrzipcode = str_replace('-', '', $nrzipcode);
+
+        $totals = $this->getProductsTotals();
+
+        if(!empty($totals)){
+            $qs = http_build_query(array(
+                "nCdEmpresa" => "",
+                "sDsSenha" => "",
+                "nCdServico" => "40010",
+                "sCepOrigem" => "84430000",
+                "sCepDestino" => $nrzipcode,
+                "nVlPeso" => $totals["vlweight"],
+                "nCdFormato" => 1,
+                "nVlComprimento" => $totals["vllength"],
+                "nVlAltura" => $totals["vlheight"],
+                "nVlLargura" => $totals["vlwidth"],
+                "nVlDiametro" => "",
+                "sCdMaoPropria" => "N",
+                "nVIValorDeclarado" => 0,
+                "sCdAvisoRecebimento" => "N",
+            ));
+
+            $xml = simplexml_load_file("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?".$qs);
+
+            var_dump($xml);
+            exit;
+        }
+        */
+
+        $this->setnrdays(12);
+        $this->setvlfreight(25.00);
+        $this->setdeszipcode($nrzipcode);
+
+        $this->save();
+    }
+
+    public function updateFreight(){
+        if(!empty($this->getdeszipcode()))
+            $this->setFreight($this->getdeszipcode());
+
+    }
+
+    public function getValues(){
+        $this->getCalculateTotal();
+
+        return parent::getValues();
+    }
+
+    public function getCalculateTotal(){
+        $this->updateFreight();
+
+        $totals = $this->getProductsTotals();
+
+        $this->setvlsubtotal($totals['vlprice'] > 0 ? $totals['vlprice'] : 0);
+        $this->setvltotal($totals['vlprice'] + $this->getvlfreight() > 0 ? $totals['vlprice'] + $this->getvlfreight() : 0);
     }
 }
 ?>
