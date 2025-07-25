e14f00a9b8c3ee66849e884af774a1e9fcd9b672
Felipe Steffen <frfelipe22@gmail.com>
2025-07-25 09:34:44 -0300

    criado cadastro de usuarios no site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -246,5 +246,31 @@ class User extends Model {
         ));
     }
 
+    public static function setErrorRegister($msg){
+        $_SESSION[User::ERROR_REGISTER] = $msg;
+    }
+
+    public static function getErrorRegister(){
+        $msg = !empty($_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+        User::clearErrorRegister();
+
+        return $msg;
+    }
+
+    public static function clearErrorRegister(){
+        $_SESSION[User::ERROR_REGISTER] = NULL;
+    }
+
+    public static function checkLoginExist($login){
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", array(
+            ":deslogin" => $login
+        ));
+
+        return !empty($results);
+    }
+
 }
 ?>
